efa11669f486749b4c19751bc9edabe0641ec314
Omid Nejadabbasi <omidne@systemgroup.net>
2024-06-04 22:13:52 +0330

    add tailwind and fix server problem

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { TextField } from "ui-commons";
+</script>
+
+<div class="border m-8"></div>
+<TextField placeholder="Hellow pos"></TextField>
