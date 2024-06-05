cc39ec997410781d4688e29469619a482f95dd49
Omid Nejadabbasi <omidne@systemgroup.net>
2024-06-06 00:36:37 +0330

    add env file and add server url

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
-  import { TextField } from "ui-commons";
+  import { serverUrl } from "$lib/http";
+  import { TextField, Button } from "ui-commons";
+
+  let password, email;
+
+  function login(){
+
+  }
 </script>
 
 <div class="border m-8"></div>
-<TextField placeholder="Hellow pos"></TextField>
+<h2>{serverUrl}</h2>
+<TextField
+  type="text"
+  label="Username"
+  placeholder="Username"
+  bind:value={email}
+/>
+
+<TextField
+  type="password"
+  label="Password"
+  placeholder="Enter yout password:"
+  bind:value={password}
+/>
+<Button on:click={login}>Login</Button>
