60a5d63b922495659acbaa4fc1081610eff0e9d0
saadati944 <saadati944@gmail.com>
2024-07-05 23:01:19 +0330

    Add itemservice

--- frontend/src/lib/data/Item.ts
+++ frontend/src/lib/data/Item.ts
@@ -1,4 +1,5 @@
 export class Item{
+    id: number;
     title: string;
     serial: string;
     description: string;
---
+++ frontend/src/lib/services/ItemService.ts
@@ -0,0 +1,44 @@
+import type { Item } from "../data/Item";
+import { HttpModule, tHttpModule } from "./http";
+import { Inject, Injectable } from "container-ioc";
+
+export const tItemService = Symbol("itemService");
+@Injectable()
+export class ItemService {
+  constructor(@Inject(tHttpModule) private http: HttpModule) {}
+  async create(item: Item){
+    const url = this.http.url("inventory/items");
+
+    return (await this.http.post(url, item)).data as Item;
+  }
+
+  async getItem(id: number){
+    const url = this.http.url("inventory/items");
+
+    return (await this.http.get(url, {params: {id}})).data as Item;
+  }
+
+  async getItems(page:number = null, pageSize:number = null){
+    const url = this.http.url("inventory/items/all")
+
+    return (await this.http.get(url, {params:{page, pageSize}})).data as Item[];
+  }
+
+  async getCount(){
+    const url = this.http.url("inventory/items/count")
+
+    return (await this.http.get(url)).data as number;
+  }
+
+  async remove(id: number){
+    const url = this.http.url("inventory/items");
+
+    await this.http.delet(url, {params: {id}});
+  }
+
+  async update(item: Item){
+    const url = this.http.url("inventory/items");
+
+    return (await this.http.put(url, item)).data as Item;
+  }
+}
